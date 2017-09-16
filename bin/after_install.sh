df4e42d8025bbd1b03f53166203d5584ca71a1a1
Kevin Chen <chen884@usc.edu>
2017-09-16 00:12:38 -0700

    put all into after_install script

--- bin/after_install.sh
+++ bin/after_install.sh
@@ -2,4 +2,5 @@ source /home/ec2-user/.bash_profile
 cd ~/
 sudo chown -R ec2-user Routiner
 cd ~/Routiner
+rm -rf node_modules
 npm install
--- bin/app_stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/
-# pkill node
-sudo chown -R ec2-user Routiner
-cd Routiner
-rm -rf node_modules
